19cf9798a6bc6b40e88075360a7eacebcc797792
Lilong Zhang <and2long@gmail.com>
2024-06-24 23:05:01 +0800

    remove unused code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:chatbotui/core/network/http.dart';
 import 'package:chatbotui/i18n/i18n.dart';
 import 'package:chatbotui/pages/home.dart';
 import 'package:chatbotui/store.dart';
@@ -9,14 +8,12 @@ import 'package:chatbotui/utils/common_util.dart';
 import 'package:chatbotui/utils/sp_util.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SPUtil.init().then((value) {
-    XHttp.init();
     runApp(Store.init(const MyApp()));
   });
   // 安卓透明状态栏
@@ -37,14 +34,6 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   @override
-  void initState() {
-    super.initState();
-    EasyLoading.instance
-      ..maskType = EasyLoadingMaskType.black
-      ..indicatorSize = 30.0;
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Consumer<LocaleStore>(
         builder: (BuildContext context, LocaleStore value, Widget? child) {
@@ -57,18 +46,14 @@ class _MyAppState extends State<MyApp> {
           GlobalMaterialLocalizations.delegate,
           GlobalCupertinoLocalizations.delegate,
           GlobalWidgetsLocalizations.delegate,
-          // 项目本地化资源代理
           S.delegate,
         ],
-        // 支持的语言
         supportedLocales: S.supportedLocales,
         locale: Locale(value.languageCode),
         home: const HomePage(),
-        builder: EasyLoading.init(
-          builder: (context, child) => GestureDetector(
-            onTap: () => CommonUtil.hideKeyboard(context),
-            child: child,
-          ),
+        builder: (context, child) => GestureDetector(
+          onTap: () => CommonUtil.hideKeyboard(context),
+          child: child,
         ),
       );
     });
